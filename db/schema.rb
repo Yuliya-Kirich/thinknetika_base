7e7b014bf4e6988f47c4ade1490182246d1d9b59
yk <yvkirich@gmail.com>
2021-02-12 00:41:14 +0300

    consolidating tables to one migrate record

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_003821) do
+ActiveRecord::Schema.define(version: 2021_01_26_215417) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2021_02_10_003821) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.integer "question_id"
-    t.index ["id"], name: "index_answers_on_id", unique: true
   end
 
   create_table "categories", force: :cascade do |t|
@@ -27,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_02_10_003821) do
     t.integer "number", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["id"], name: "index_categories_on_id", unique: true
+    t.index ["type"], name: "index_categories_on_type", unique: true
   end
 
   create_table "questions", force: :cascade do |t|
@@ -35,7 +34,6 @@ ActiveRecord::Schema.define(version: 2021_02_10_003821) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "test_id"
-    t.index ["id"], name: "index_questions_on_id", unique: true
   end
 
   create_table "tests", force: :cascade do |t|
@@ -43,8 +41,9 @@ ActiveRecord::Schema.define(version: 2021_02_10_003821) do
     t.integer "level", default: 1, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "deleted_at"
     t.integer "category_id"
-    t.index ["id"], name: "index_tests_on_id", unique: true
+    t.index ["title"], name: "index_tests_on_title", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,7 +51,6 @@ ActiveRecord::Schema.define(version: 2021_02_10_003821) do
     t.string "last_name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["id"], name: "index_users_on_id", unique: true
   end
 
   add_foreign_key "answers", "questions"
