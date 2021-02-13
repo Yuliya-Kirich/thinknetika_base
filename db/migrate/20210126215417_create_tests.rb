c8304b4369c3e71ad0d22ce688744f033620d416
yk <yvkirich@gmail.com>
2021-02-13 21:05:45 +0300

    Revert "consolidating tables to one migrate record"
    This reverts commit 7e7b014bf4e6988f47c4ade1490182246d1d9b59.

--- db/migrate/20210126215417_create_tests.rb
+++ db/migrate/20210126215417_create_tests.rb
@@ -1,52 +1,10 @@
 class CreateTests < ActiveRecord::Migration[6.0]
   def change
+    create_table :tests do |t|
+      t.string :title
+      t.integer :level
 
-
-      create_table "tests", force: :cascade do |t|
-        t.string "title", null: false
-        t.integer "level", default: 1, null: false
-        t.datetime "created_at", precision: 6, null: false
-        t.datetime "updated_at", precision: 6, null: false
-        t.datetime "deleted_at"
-        t.integer "category_id"
-      end
-      add_index(:tests, :title, unique: true)
-
-
-      create_table "categories", force: :cascade do |t|
-        t.string "type", null: false
-        t.integer "number", null: false
-        t.datetime "created_at", precision: 6, null: false
-        t.datetime "updated_at", precision: 6, null: false
-      end
-      add_index(:categories, :type, unique: true)
-
-      create_table "answers", force: :cascade do |t|
-        t.string "body", null: false
-        t.boolean "correct", default: true, null: false
-        t.datetime "created_at", precision: 6, null: false
-        t.datetime "updated_at", precision: 6, null: false
-        t.integer "user_id"
-        t.integer "question_id"
-      end
-
-      create_table "questions", force: :cascade do |t|
-        t.string "body", null: false
-        t.datetime "created_at", precision: 6, null: false
-        t.datetime "updated_at", precision: 6, null: false
-        t.integer "test_id"
-      end
-
-      create_table "users", force: :cascade do |t|
-        t.string "first_name", null: false
-        t.string "last_name", null: false
-        t.datetime "created_at", precision: 6, null: false
-        t.datetime "updated_at", precision: 6, null: false
-      end
-
-      add_foreign_key "answers", "questions"
-      add_foreign_key "answers", "users"
-      add_foreign_key "questions", "tests"
-      add_foreign_key "tests", "categories"
+      t.timestamps
+    end
   end
 end
