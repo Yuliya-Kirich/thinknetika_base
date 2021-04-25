8f54141db916715309d455d97412af9e1094ec0d
Yuliya Kirich <yvkirich@gmail.com>
2021-04-25 23:17:08 +0300

    fix 3 - with Users_sessions table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  def initialize(current_user)
+  @current_user = current_user
+  end
+
+  def search_test(level)
+    Test.joins('JOIN users_sessions on users_sessions.test_id=tests.id').where('level=?', level).where('user_id==?', @current_user)
+  end
 end
+
---
+++ db/migrate/20210222214056_rename_column_to_categories.rb
@@ -0,0 +1,5 @@
+class RenameColumnToCategories < ActiveRecord::Migration[6.0]
+  def change
+    rename_column(:categories , :type, :type_of_category)
+  end
+end
