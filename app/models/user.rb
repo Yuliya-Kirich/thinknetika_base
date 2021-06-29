bf633d4e4630d30fd8dd9652a70404c9377e3c3b
Yuliya Kirich <yvkirich@gmail.com>
2021-06-30 00:49:51 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :users_sessions
   has_many :tests, through: :users_sessions
-  has_many :tests, foreign_key: 'author'
+  has_many :tests, foreign_key: :user_id
 
   def search_test(level)
     Test.joins('JOIN users_sessions on users_sessions.test_id=tests.id').where('level=?', level).where('user_id = ?', self.id).pluck(:title)
