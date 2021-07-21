8afdbca5ae64f0334e6fa63d5faf690f70b8dc62
Yuliya Kirich <yvkirich@gmail.com>
2021-07-22 00:18:51 +0300

    scope methods

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,5 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  scope :correct, -> {where(correct:1).order(created_at: :asc)}
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,12 @@ class Test < ApplicationRecord
   belongs_to :author, foreign_key: :user_id, class_name: 'User'
   belongs_to :category
 
+  n = Float::INFINITY
+
+  scope :easy, -> {where(level:0..1).order(created_at: :asc)}
+  scope :middle, -> {where(level:2..4).order(created_at: :asc)}
+  scope :difficult, -> {where(level:5..n).order(created_at: :asc)}
+
   def self.search_to_categories_title(type_of_category)
     Test.joins('JOIN categories on tests.category_id=categories.id')
         .where('type_of_category=?' , type_of_category)
