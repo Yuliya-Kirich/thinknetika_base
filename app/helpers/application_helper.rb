472e4cfe9910c21c54942f11767cb6986706c7f9
Yuliya Kirichenko <yvkirich@gmail.com>
2022-10-20 03:07:29 +0300

    1. implemented all standard actions and views for resource question. 2. added footer, which displays the current year, author's name, project name. 3.added a helper for the title of the page for creating and editing a question. Added date helper in footer. Added helper with links to author and project.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def footer_year
+    content_tag(:p,  "2021 - #{Time.now.strftime('%Y')}")
+  end
+  def github_url(author, repo)
+    "<p style='text-align: center'><a href='#{author}'>Информация об авторе: Кириченко Юлия. </a><a href='#{repo}'>проект Тест Гуру</a></p>".html_safe
+  end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,5 @@
 module QuestionsHelper
+  def question_header
+    content_tag(:div, content_tag(:h1,  "#{t action_name} вопрос для теста: #{@test.title} "), class: "strong")
+  end
 end
