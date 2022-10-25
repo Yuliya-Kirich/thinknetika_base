7d6b2e2206dc26550c3367acfbc6b4408e0258c7
Yuliya Kirichenko <yvkirich@gmail.com>
2022-10-25 03:44:30 +0300

    fix routes

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,10 +2,10 @@ module QuestionsHelper
   def question_header
     content_tag(:div, content_tag(:h1,
                                   "#{t action_name} вопрос для теста:
-      #{if @question.blank?
-        @question.test.title
-          else
-        @test.title
+      #{if @test.blank?
+          @question.test.title
+        else
+          @test.title
       end} "), class: "strong")
   end
 end
